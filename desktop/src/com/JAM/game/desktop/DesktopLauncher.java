34eee144ba4b8fa07c9f4fa8e337f9d76fd003e8
HappySaila <granthaldanewilson836@gmail.com>
2016-08-24 11:01:30 +0200

    Init JAM

---
+++ desktop/src/com/JAM/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.JAM.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.JAM.game.JamDriver;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JamDriver(), config);
+	}
+}
